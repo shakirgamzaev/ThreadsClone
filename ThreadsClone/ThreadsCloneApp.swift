879aa0bdba49043b3fafd1b97b88a7a40a5dfcf1
Shakir Gamzaev <shkir100@gmail.com>
2025-12-16 15:13:38 +0100

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by shakir Gamzaev on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
